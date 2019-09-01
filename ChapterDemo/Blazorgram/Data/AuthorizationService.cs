ca46c8ef2eb02414dc5c390e20e5fd21c8534490
Erlend Rognes <erlend.rognes@knowit.no>
2019-09-01 17:00:42 +0200

    Start adding login functionality

---
+++ ChapterDemo/Blazorgram/Data/AuthorizationService.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Threading.Tasks;
+
+//namespace BlazorgramDemo.Data
+//{
+//    public class AuthorizationService
+//    {
+//        public Task Login()
+//        {
+//            Task.Run(() => Console.Read());
+//        }
+
+//        public Task LogOut() => AuthStateProvider.SetTokenAsync(null);
+        
+//    }
+//}
